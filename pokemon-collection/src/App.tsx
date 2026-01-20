8b06392eed80c3bf9600e315ab47bd2b3588287a
Efe Onota <eonota@rrc.ca>
2026-01-19 22:38:30 -0600

    Added footer component with team member names and site links

--- pokemon-collection/src/App.tsx
+++ pokemon-collection/src/App.tsx
@@ -1,14 +1,21 @@
-import './App.css'
-import { CardList } from './components/card.tsx'
-import Header from './components/common/header/Header.tsx'
+import "./App.css";
+
+import { CardList } from "./components/card.tsx";
+import Header from "./components/common/header/Header.tsx";
+import Footer from "./components/common/footer/Footer.tsx";
 
 function App() {
   return (
     <>
       <Header />
-      <CardList/>
+      
+      <main>
+        <CardList />
+      </main>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
