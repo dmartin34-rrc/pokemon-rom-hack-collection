137e043c394a254c4a6f7063b26f6746441d7174
Zachary Lam <zachary74m@outlook.com>
2026-02-01 20:53:25 -0600

    Changed CardList component to incorporate Tailwind

--- pokemon-collection/src/components/card/card.tsx
+++ pokemon-collection/src/components/card/card.tsx
@@ -1,23 +1,22 @@
 import cards from '../../data/card.json';
 import type { Card } from '../../types/cards';
-import './Card.css';
 
 export function CardList() {
   return cards.map((card) => {
     return (
-      <div className="card-container" key={card.title}>
-        <div className="feature-rom-container">
-          <div className="feature-rom">
-            <a className="feature-rom-link">
+      <div className="w-[400px]" key={card.title}>
+        <div>
+          <div className="relative">
+            <a>
               <CardImage card={card} />
             </a>
             <CardTag card={card} />
           </div>
         </div>
-        <div className="rom-title-container">
+        <div>
           <CardTitle card={card} />
         </div>
-        <div className="rom-description">
+        <div>
           <CardDescription card={card} />
         </div>
       </div>
@@ -34,10 +33,10 @@ export function Card({ title }: Pick<Card, 'title'>) {
 
   return (
     <>
-      <div className="card-container">
-        <div className="feature-rom-container">
-          <div className="feature-rom">
-            <a className="feature-rom-link">
+      <div className="w-[400px]">
+        <div>
+          <div className="relative">
+            <a>
               <CardImage card={card} />
             </a>
             <span>
@@ -45,10 +44,10 @@ export function Card({ title }: Pick<Card, 'title'>) {
             </span>
           </div>
         </div>
-        <div className="rom-title-container">
+        <div>
           <CardTitle card={card} />
         </div>
-        <div className="rom-description">
+        <div>
           <CardDescription card={card} />
         </div>
       </div>
@@ -57,13 +56,19 @@ export function Card({ title }: Pick<Card, 'title'>) {
 }
 
 function CardImage({ card }: { card: Card }) {
-  return <img className="rom-image" src={card.img} alt={card.title} />;
+  return (
+    <img
+      className="rounded-[15px] w-[400px] h-[250px]"
+      src={card.img}
+      alt={card.title}
+    />
+  );
 }
 
 function CardTitle({ card }: { card: Card }) {
   return (
-    <h3 className="rom-title">
-      <a className="title-link">{card.title}</a>
+    <h3>
+      <a>{card.title}</a>
       <span className="bookmark"></span>
     </h3>
   );
@@ -71,9 +76,13 @@ function CardTitle({ card }: { card: Card }) {
 
 function CardTag({ card }: { card: Card }) {
   return (
-    <aside className="rom-tags">
+    <aside className="absolute top-40 left-4 bottom-4 w-[380px] flex items-end flex-wrap gap-2">
       {card.tags.map((tag, index) => (
-        <a href="#" key={index}>
+        <a
+          href="#"
+          key={index}
+          className="bg-slate-500 text-white px-[0.4rem] py-[0.1rem] rounded-[5px] tracking-[1px] hover:bg-[#475569]"
+        >
           {tag}
         </a>
       ))}
