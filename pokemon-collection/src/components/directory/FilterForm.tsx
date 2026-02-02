275d157423b5e321635dfcfe82bca05efb449174
Zachary Lam <zachary74m@outlook.com>
2026-02-02 13:57:07 -0600

    Implemented basic layout for form component on the Rom Directory page

---
+++ pokemon-collection/src/components/directory/FilterForm.tsx
@@ -0,0 +1,32 @@
+const FilterForm = () => {
+  return (
+    <form>
+      <label>
+        <span>Title</span>
+        <input type="text" placeholder="PokeSouls" />
+      </label>
+      <label>
+        <span>Tags</span>
+        <input type="text" placeholder="X, Red, Beta" />
+      </label>
+      <label>
+        <span>Year</span>
+        <div>
+          <input type="number" />
+          <span> to </span>
+          <input type="number" />
+        </div>
+      </label>
+      <label>
+        <input type="checkbox" />
+        <span>Multiplayer</span>
+      </label>
+      <label>
+        <input type="checkbox" />
+        <span>Completed</span>
+      </label>
+    </form>
+  );
+};
+
+export default FilterForm;
---
+++ pokemon-collection/src/components/directory/RomDirectory.tsx
@@ -0,0 +1,12 @@
+// components
+import FilterForm from './FilterForm';
+
+const RomDirectory = () => {
+  return (
+    <div>
+      <FilterForm />
+    </div>
+  );
+};
+
+export default RomDirectory;
