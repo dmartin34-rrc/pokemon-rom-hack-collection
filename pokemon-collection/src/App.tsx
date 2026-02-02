0f332bd0fea5017be78bd753fb67b22950f29607
zachary-lam <169072342+zachary-lam@users.noreply.github.com>
2026-02-01 20:22:52 -0600

    Clean up imports in App.tsx
    Removed unused imports for CardList, Header, and Footer components.

--- pokemon-collection/src/App.tsx
+++ pokemon-collection/src/App.tsx
@@ -2,9 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 // components
 import Layout from './components/common/Layout';
-import { CardList } from './components/card.tsx'
-import Header from './components/common/header/Header.tsx'
-import Footer from './components/common/footer/footer.tsx'
 
 function App() {
   return (
