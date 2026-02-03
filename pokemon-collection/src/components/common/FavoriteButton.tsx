e7d0ea22156bd1e98bb20354a3d6b53a9ddec5fe
Dean <dmartin34@academic.rrc.ca>
2026-02-02 21:59:22 -0600

    Added: color change to favorite button

--- pokemon-collection/src/components/common/FavoriteButton.tsx
+++ pokemon-collection/src/components/common/FavoriteButton.tsx
@@ -15,7 +15,7 @@ export function FavoriteButton(
             }}
             className={`
             ml-2 text-xl leading-none transition-colors duration-200 
-            ${isFavorite ? 'text-yellow-500' : 'text-gray-300 hover:text-gray-400'}`}
+            ${isFavorite ? 'text-yellow-500' : 'text-yellow-500 hover:text-gray-400'}`}
         >
             {isFavorite ? '★': '☆'}
         </button>
