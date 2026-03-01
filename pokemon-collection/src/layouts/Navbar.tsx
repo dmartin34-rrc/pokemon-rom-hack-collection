7d11fb1e52c53176eb023982e593085506c01d43
Zachary Lam <zachary74m@outlook.com>
2026-03-01 12:35:23 -0600

    Updated navbar to have pseudoclasses to be more UI friendly on hover, active, etc

--- pokemon-collection/src/layouts/Navbar.tsx
+++ pokemon-collection/src/layouts/Navbar.tsx
@@ -1,19 +1,26 @@
 import { NavLink } from 'react-router';
 
 const Navbar = () => {
+  const pseudoClass = ({ isActive }: { isActive: boolean }): string =>
+    `px-2 py-2 font-medium text-xl cursor-pointer border-b-2 ${
+      isActive
+        ? 'text-slate-900 border-slate-500'
+        : 'text-slate-400 border-transparent hover:text-slate-600 hover:border-slate-200 active:border-red-300'
+    }`;
+
   return (
     <nav>
-      <div>
-        <NavLink to="/" end>
+      <div className="flex gap-1 px-4 py-2">
+        <NavLink to="/" end className={pseudoClass}>
           Home
         </NavLink>
-        <NavLink to="/favorites" end>
+        <NavLink to="/favorites" end className={pseudoClass}>
           Favorites
         </NavLink>
-        <NavLink to="/directory">
+        <NavLink to="/directory" className={pseudoClass}>
           Directory
         </NavLink>
-        <NavLink to="/tracker">
+        <NavLink to="/tracker" className={pseudoClass}>
           Tracker
         </NavLink>
       </div>
