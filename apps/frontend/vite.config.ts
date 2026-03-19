b44eb8d2abf3579a278abe7579c03e379fbb9c6c
Zachary Lam <zachary74m@outlook.com>
2026-03-19 16:16:38 -0500

    chore(frontend): update frontend dependencies
    - configured tsconfig files
    - configured eslint config file
    - configured vite config file

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,10 @@
+import { ViteUserConfig, defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()] as ViteUserConfig['plugins'],
+  test: {
+    environment: 'jsdom',
+  },
+});
