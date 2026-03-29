d7e2717db1810c9f6b87a227879af330381c830f
Zachary Lam <zlam@rrc.ca>
2026-03-29 08:50:37 -0500

    refactor(backend): prisma schema

---
+++ apps/backend/prisma/migrations/20260329135012_itemlist/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "ROM" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "tags" TEXT[],
+    "img" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL,
+    "multiplayer" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ROM_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ItemList" (
+    "id" SERIAL NOT NULL,
+    "page" TEXT NOT NULL,
+    "romId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ItemList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ItemList_page_romId_key" ON "ItemList"("page", "romId");
+
+-- AddForeignKey
+ALTER TABLE "ItemList" ADD CONSTRAINT "ItemList_romId_fkey" FOREIGN KEY ("romId") REFERENCES "ROM"("id") ON DELETE CASCADE ON UPDATE CASCADE;
