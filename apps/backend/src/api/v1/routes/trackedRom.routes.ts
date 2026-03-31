05f9c271eb2a2fc4af576b59a3e4119dc5b18ba1
Efe Onota <eonota@rrc.ca>
2026-03-30 23:30:50 -0500

    mounted routes

--- apps/backend/src/api/v1/routes/trackedRom.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as trackedRomController from '../controllers/trackedRom.controller';
-
-const router = Router();
-
-router.get('/', trackedRomController.getAllTrackedRoms);
-router.post('/', trackedRomController.createTrackedRom);
-
-export default router;
