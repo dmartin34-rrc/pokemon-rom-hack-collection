36b2ed0e7fd31618593e898e62c0607924ee2b12
Dean Martin <dmartin34@academic.rrc.ca>
2026-04-20 04:42:16 -0500

    feat(frontend): implement clerk auth UI in navbar

--- apps/frontend/src/layouts/Navbar.tsx
+++ apps/frontend/src/layouts/Navbar.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router';
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
   const pseudoClass = ({ isActive }: { isActive: boolean }): string =>
@@ -11,18 +12,40 @@ const Navbar = () => {
   return (
     <nav>
       <div className="flex gap-1 px-4 py-2">
+
+        {/* Main Nav Items */}
         <NavLink to="/" end className={pseudoClass}>
           Home
         </NavLink>
-        <NavLink to="/favorites" end className={pseudoClass}>
-          Favorites
-        </NavLink>
+
         <NavLink to="/directory" className={pseudoClass}>
           Directory
         </NavLink>
+
         <NavLink to="/tracker" className={pseudoClass}>
           Tracker
         </NavLink>
+        
+        {/* Show when SIGNED IN */}
+        <SignedIn>
+          <NavLink to="/favorites" end className={pseudoClass}>
+            Favorites
+          </NavLink>
+        </SignedIn>
+
+        {/* NOTE: this component is only visible when the user is not signed in and comes from clerk */}
+        <div className="ml-auto flex items-center">
+          <SignedOut>
+            <NavLink to="/login" className={pseudoClass}>
+              Login
+            </NavLink>
+          </SignedOut>
+        </div>
+
+        <SignedIn>
+          <UserButton afterSignOutUrl="/"/>
+        </SignedIn>
+
       </div>
     </nav>
   );
