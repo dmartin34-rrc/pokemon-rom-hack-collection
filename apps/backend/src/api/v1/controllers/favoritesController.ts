90cade3a763603633e0fb1575c9b43a7dc3366d2
Zachary Lam <zlam@rrc.ca>
2026-04-23 18:20:50 -0500

    Updated services and controllers to pass userId, userRepo, and included mock user data to test backend functionality via HTTP calls in Postman

--- apps/backend/src/api/v1/controllers/favoritesController.ts
+++ apps/backend/src/api/v1/controllers/favoritesController.ts
@@ -1,6 +1,9 @@
-import { Request, Response, NextFunction } from "express";
-import * as favoritesService from "../services/favoritesService";
-import { successResponse } from "../models/responseModel";
+import { Request, Response, NextFunction } from 'express';
+import * as favoritesService from '../services/favoritesService';
+import { successResponse } from '../models/responseModel';
+
+// TEST USER, DELETE ONE CLERK WORKFLOW IS FULLY SETUP
+const TEST_USER = Number(process.env.TEST_USER ?? 1);
 
 /**
  * Controller methods determine how to handle requests and respond to requests.
@@ -8,44 +11,44 @@ import { successResponse } from "../models/responseModel";
  * and responds to the request with an appropriate code/body.
  */
 
-export const getFavorites = async(
+export const getFavorites = async (
   _req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): Promise<void> => {
-  try{
-    const favorites = await favoritesService.getAllFavorites();
-    res.status(200).json(
-      successResponse(favorites, "Favorites retrieved successfully")
-    );
+  try {
+    const favorites = await favoritesService.getAllFavorites(TEST_USER);
+    res
+      .status(200)
+      .json(successResponse(favorites, 'Favorites retrieved successfully'));
   } catch (error) {
     // errorHandler middleware will always be the last to catch error throws
     next(error);
   }
 };
 
-export const toggleFavorite = async(
+export const toggleFavorite = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): Promise<void> => {
   try {
     const romId = req.body.romId;
 
-    const result = await favoritesService.toggleFavorite(romId);
+    const result = await favoritesService.toggleFavorite(romId, TEST_USER);
 
     if (result === null) {
       // removed
-      res.status(200).json(
-        successResponse(null, "Favorite removed successfully")
-      );
+      res
+        .status(200)
+        .json(successResponse(null, 'Favorite removed successfully'));
     } else {
       // added
-      res.status(201).json(
-        successResponse(result, "Favorite added successfully")
-      );
+      res
+        .status(201)
+        .json(successResponse(result, 'Favorite added successfully'));
     }
   } catch (error) {
     next(error);
   }
-}
+};
